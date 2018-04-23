3e69bd198814080e76fc711d7b184e7f9a4d0285
acrops <975592789+acrops@users.noreply.github.com>
2018-04-23 16:09:02 +0800

    The first update

--- 任意个数整型数排序/任意个数整型数排序/源.cpp
+++ 任意个数整型数排序/任意个数整型数排序/源.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void show(vector<int>&x, vector<int>::iterator&p)
+void show(vector<int>&x, vector<int>::iterator& p)
 {
 	int i = 0;
 	auto MaxPosition = max_element(x.begin(), x.end());//求最大值
@@ -17,7 +17,7 @@ void show(vector<int>&x, vector<int>::iterator&p)
 		temp++;
 	}
 	temp++;
-	for (p = x.begin(); p < x.end(); p++)//每十个一行进行输出，输出格式右对齐，输出宽度按最大值位数+1
+	for (p = x.begin(); p != x.end(); p++)//每十个一行进行输出，输出格式右对齐，输出宽度按最大值位数+1
 	{
 		cout << setiosflags(ios::right) << setw(temp) << *p;
 		i++;
@@ -38,17 +38,18 @@ int main()
 {
 	vector<int> x;
 	int temp;
-	cout << "输入任意整数，系统将进行排序" << endl<<"输入0进行升序排列"<<endl<<"输入1进行降序排列"<<endl;
-	int ans=0;
-	cin >> ans;
+	cout << "输入任意整数，系统将进行排序" << endl<<"输入A进行升序排列"<<endl<<"输入D进行降序排列"<<endl;
+	char ans = 'A';
+	cin.get(ans);
+	if(ans!='A'&&ans!='D') cout << "错误的输入，默认进行升序" << endl;
 	cout << "请输入数据(以任意字母结束）:" << endl;
 	while (cin >> temp)//数据输入
 	{
 		x.push_back(temp);
 	}
 	vector<int>::iterator p;
-	if(ans==0)	sort(x.begin(), x.end());//默认升序
-	else sort(x.begin(), x.end(), comp);//降序排列
+	if(ans=='D')	sort(x.begin(), x.end(), comp);//降序排列
+	else	sort(x.begin(), x.end());//默认升序
 	show(x, p);
 	return 0;
 }
